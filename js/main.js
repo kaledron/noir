44ec394887a35e2502a327ba8d2e4b68c87ccaa7
Kristijan <kaledron@yahoo.com>
2019-09-23 19:32:57 +0200

    home page in progress

--- js/main.js
+++ js/main.js
@@ -6,26 +6,43 @@ if (window.innerWidth > 1024 && window.innerHeight > 599 || window.innerWidth > 
 
 
     var video = document.getElementById('video');
-    video.controls = false;
-    video.loop = true;
-    video.muted = false;
-    video.preload = true;
-    video.play();
-    var playPromise = video.play();
-    setTimeout(() => playPromise.then(() => video.play()).catch(() => "", 29));
 } else {
     document.getElementById("2").innerText = "sdfasdfa";
 
     var video = document.getElementById('video-m');
-    video.controls = false;
-    video.loop = true;
-    video.muted = false;
-    video.preload = true;
-    video.play();
-    var playPromise = video.play();
-    setTimeout(() => playPromise.then(() => video.play()).catch(() => "", 29));
-    
 }
+
+// video.controls = false;
+// video.loop = true;
+// video.muted = false;
+// // video.preload = true;
+// // video.click();
+// // video.play();
+// var playPromise = video.play();
+// setTimeout(() => playPromise.then(_ => {} ).catch(() => "", 29));
+
+
+var t1 = 29001; // / If polling, this time must be longer than the length of the video. In the case of webscoket, a status play should be set to avoid repetition, as follows:
+var t2 = 29000; // video length to ensure full playback to users
+var play = false;
+function run(){
+    if (play) {
+        return false;
+    }
+    video.currentTime = 0; // Sets the start time of the playing video
+    video.muted = false; // Turn off silent state
+    video.volume = 0.9; // Set the sound size of the video
+    play = true;
+    setTimeout(function(){
+        play = false;
+        video.muted = true; // After playing, turn on the mute state
+    },t2);
+}
+setInterval(function(){
+    run(); // pretend to be polling server or pull data from websocket
+},t1);
+
+
 // if (playPromise !== undefined) {
 //     playPromise.then(function() {
 //         setTimeout(30);
