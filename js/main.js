4f509d46a1c0a78049a2f433c50e60da38783723
Kristijan <kaledron@yahoo.com>
2019-10-03 10:47:09 +0200

    added partiles animation on every white image background

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-if (window.innerWidth > 1024 && window.innerHeight > 599 || window.innerWidth > 1079 && window.innerHeight < 1800) {} else {
-    document.getElementsByClassName('main')[0].style.display = 'none'; 
-    document.getElementsByClassName('main-m')[0].style.display = 'block'; 
-}
+// if (window.innerWidth > 1024 && window.innerHeight > 599 || window.innerWidth > 1079 && window.innerHeight < 1800) {} else {
+//     document.getElementsByClassName('main')[0].style.display = 'none'; 
+//     document.getElementsByClassName('main-m')[0].style.display = 'block'; 
+// }
