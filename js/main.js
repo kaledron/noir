5ba1ed08e74df6711719b7877aaf04ad5eab91e0
Kristijan <kaledron@yahoo.com>
2019-10-06 12:03:18 +0200

    added home mobile page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-// if (window.innerWidth > 1024 && window.innerHeight > 599 || window.innerWidth > 1079 && window.innerHeight < 1800) {} else {
-//     document.getElementsByClassName('main')[0].style.display = 'none'; 
-//     document.getElementsByClassName('main-m')[0].style.display = 'block'; 
-// }
+if (window.innerWidth < 1081) {
+    v_type = document.getElementById('video').currentSrc.split('/');
+    v_type = v_type[v_type.length-1];
+    v_type = v_type.split('.')[1];
+    document.getElementById("video").src = "img/noir-m."+v_type;
+    console.log(document.getElementById("video"));
+    // alert('Screen Widht: ' + window.innerWidth + "\n Screen Height1: " + window.innerHeight);
+}
