d03b1dae68b1f89e3d7ad869e8d400beaf95a353
Kristijan <kaledron@yahoo.com>
2019-10-08 14:41:42 +0200

    final mods on eco page segment

--- js/particles_init.js
+++ js/particles_init.js
@@ -1,7 +1,5 @@
 if (window.innerWidth < 1081) {
-    console.log('M');
     particlesJS.load('main-id', 'js/particles-m.json');
 } else {
-    console.log('not M');
     particlesJS.load('main-id', 'js/particles.json');
 }
